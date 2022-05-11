3d89152c1110179d22c50c25c542d499f191e3c5
Niko <nikobo2003@gmail.com>
2022-05-11 14:59:17 +0200

    changed coroutine use

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,8 @@
 import com.google.gson.Gson
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import messagetypes.Message
 import messagetypes.Response
 import mu.KotlinLogging
@@ -21,7 +24,7 @@ val storagePath: File = File("storage")
 val storageFile: File = File(storagePath.path + File.separator + "storageFile.json")
 val logger = KotlinLogging.logger {}
 
-fun main(args: Array<String>) {
+fun main(args: Array<String>) = runBlocking {
     // TODO handle remaining cases (numpad open(coroutine/second thread)), store pin hashed, open method
     System.setProperty("javax.net.ssl.keyStore", "certificates" + File.separator + "dooropenercertificate.pfx")
     System.setProperty("javax.net.ssl.keyStorePassword", "dooropenerpassword")
@@ -37,88 +40,8 @@ fun main(args: Array<String>) {
         val socket = serverSocket.accept()
         println("Connection from: " + socket.inetAddress)
         socket.soTimeout = 1500
-        GlobalScope.launch(Dispatchers.IO) {
-            val message: Message = Gson().fromJson(
-                BufferedReader(InputStreamReader(socket.getInputStream())).readLine(), Message::class.java)
-            var response = Response()
-            if (message.type == "login") {
-                response = handleLogin(message, socket)
-            } else {
-                if (authenticateToken(message)) {
-                    when (message.type) {
-                        "open" -> {
-                            open()
-                            response.text = "Success!"
-                            response.internalMessage = "success"
-                            logger.info(socket.inetAddress.toString() + " opened the door")
-                        }
-                        "keypadConfig" -> {
-                            storage.isKeypadEnabled = (message.content!!.toInt() in 1 until 10)
-                            storage.keypadTime = message.content.toInt()
-                            response.text = "Saved config! :D"
-                            response.internalMessage = "success"
-                            logger.info(socket.inetAddress.toString() + " changed the keypad configuration")
-                        }
-                        "otpAdd" -> {
-                            val tempOtp: Otp = Gson().fromJson(message.content, Otp::class.java)
-                            storage.otps[tempOtp.pin] = LocalDate.parse(tempOtp.expirationDate)
-                            response.text = "Saved OTP! :D"
-                            response.internalMessage = "success"
-                            logger.info(socket.inetAddress.toString() + " added a new OTP")
-                        }
-                        "otpRemove" -> {
-                            storage.otps.remove(Gson().fromJson(message.content, Otp::class.java).pin)
-                            response.text = "Removed OTP! :O"
-                            response.internalMessage = "success"
-                            logger.info(socket.inetAddress.toString() + " removed an OTP")
-                        }
-                        "keepLogs" -> {
-                            storage.keepLogs = message.content!!.lowercase().contains("true")
-                            response.text = "Saved preference! :D"
-                            response.internalMessage = "success"
-                            logger.info(socket.inetAddress.toString() + " changed the keeping of logs to ${storage.keepLogs}")
-                        }
-                        "changePin" -> {
-                            storage.pin = Integer.parseInt(message.content)
-                            storage.tokens.clear()
-                            response.text = "Changed pin! :D"
-                            response.internalMessage = "success"
-                            logger.info(socket.inetAddress.toString() + " changed the pin")
-                        }
-                        "globalLogout" -> {
-                            storage.tokens.clear()
-                            response.text = "Logged everyone out! :/"
-                            response.internalMessage = "success"
-                            logger.info(socket.inetAddress.toString() + " logged everyone out")
-                        }
-                        "reset" -> {
-                            storage = Storage()
-                            File("LogFile.log").writeText("")
-                            response.text = "Ready for a new start :D"
-                            response.internalMessage = "success"
-                            logger.warn(socket.inetAddress.toString() + " reset this device")
-                            logger.info(socket.inetAddress.toString() + " Ready for a new start :D")
-                        }
-                        "requestLogs" -> {
-                            val logArray: Array<String> = File("LogFile.log").readLines().toTypedArray()
-                            response.text = Gson().toJson(logArray)
-                            println(response.text)
-                            response.internalMessage = "success"
-                            logger.info(socket.inetAddress.toString() + " requested the logs")
-                        }
-                    }
-                } else {
-                    response.text = "Invalid token :C Login again!"
-                    response.internalMessage = "invalid token"
-                    logger.warn(socket.inetAddress.toString() + " tried logging in with an invalid token")
-                }
-            }
-
-            withContext(Dispatchers.IO) {
-                PrintWriter(socket.getOutputStream(), true).println(Gson().toJson(response))
-                socket.close()
-            }
-            storageFile.writeText(Gson().toJson(storage))
+        launch(Dispatchers.IO) {
+            handleConnection(socket)
         }
     }
 }
@@ -144,6 +67,91 @@ fun createServerSocket(): SSLServerSocket {
     serverSocket.enabledProtocols = arrayOf("TLSv1.3")
     serverSocket.enabledCipherSuites = arrayOf("TLS_AES_256_GCM_SHA384")
     return serverSocket
+}
+
+suspend fun handleConnection(socket: Socket) {
+    val message: Message = Gson().fromJson(
+        BufferedReader(InputStreamReader(socket.getInputStream())).readLine(), Message::class.java
+    )
+    var response = Response()
+    if (message.type == "login") {
+        response = handleLogin(message, socket)
+    } else {
+        if (authenticateToken(message)) {
+            when (message.type) {
+                "open" -> {
+                    open()
+                    response.text = "Success!"
+                    response.internalMessage = "success"
+                    logger.info(socket.inetAddress.toString() + " opened the door")
+                }
+                "keypadConfig" -> {
+                    storage.isKeypadEnabled = (message.content!!.toInt() in 1 until 10)
+                    storage.keypadTime = message.content.toInt()
+                    response.text = "Saved config! :D"
+                    response.internalMessage = "success"
+                    logger.info(socket.inetAddress.toString() + " changed the keypad configuration")
+                }
+                "otpAdd" -> {
+                    val tempOtp: Otp = Gson().fromJson(message.content, Otp::class.java)
+                    storage.otps[tempOtp.pin] = LocalDate.parse(tempOtp.expirationDate)
+                    response.text = "Saved OTP! :D"
+                    response.internalMessage = "success"
+                    logger.info(socket.inetAddress.toString() + " added a new OTP")
+                }
+                "otpRemove" -> {
+                    storage.otps.remove(Gson().fromJson(message.content, Otp::class.java).pin)
+                    response.text = "Removed OTP! :O"
+                    response.internalMessage = "success"
+                    logger.info(socket.inetAddress.toString() + " removed an OTP")
+                }
+                "keepLogs" -> {
+                    storage.keepLogs = message.content!!.lowercase().contains("true")
+                    response.text = "Saved preference! :D"
+                    response.internalMessage = "success"
+                    logger.info(socket.inetAddress.toString() + " changed the keeping of logs to ${storage.keepLogs}")
+                }
+                "changePin" -> {
+                    storage.pin = Integer.parseInt(message.content)
+                    storage.tokens.clear()
+                    response.text = "Changed pin! :D"
+                    response.internalMessage = "success"
+                    logger.info(socket.inetAddress.toString() + " changed the pin")
+                }
+                "globalLogout" -> {
+                    storage.tokens.clear()
+                    response.text = "Logged everyone out! :/"
+                    response.internalMessage = "success"
+                    logger.info(socket.inetAddress.toString() + " logged everyone out")
+                }
+                "reset" -> {
+                    storage = Storage()
+                    File("LogFile.log").writeText("")
+                    response.text = "Ready for a new start :D"
+                    response.internalMessage = "success"
+                    logger.warn(socket.inetAddress.toString() + " reset this device")
+                    logger.info(socket.inetAddress.toString() + " Ready for a new start :D")
+                }
+                "requestLogs" -> {
+                    val logArray: Array<String> = File("LogFile.log").readLines().toTypedArray()
+                    response.text = Gson().toJson(logArray)
+                    println(response.text)
+                    response.internalMessage = "success"
+                    logger.info(socket.inetAddress.toString() + " requested the logs")
+                }
+            }
+        } else {
+            response.text = "Invalid token :C Login again!"
+            response.internalMessage = "invalid token"
+            logger.warn(socket.inetAddress.toString() + " tried logging in with an invalid token")
+        }
+    }
+
+    withContext(Dispatchers.IO) {
+        PrintWriter(socket.getOutputStream(), true).println(Gson().toJson(response))
+        socket.close()
+    }
+    storageFile.writeText(Gson().toJson(storage))
 }
 
 fun handleLogin(message: Message, socket: Socket): Response {
